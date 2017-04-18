177e3149c437b5ec1f289474e7817aa71c90b2d3
Furiml <a.screve.94801@gmail.com>
2017-04-18 17:05:27 +0200

    Add BottleAction model & controller, but have a permission issue

---
+++ api/controllers/BottleActionController.js
@@ -0,0 +1,109 @@
+/**
+ * BottleActionController
+ *
+ * @description :: Server-side logic for managing BottleActions
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+    /**
+     * @api {get} /bottleAction/find Find all bottles actions related to the bar and subscribe to them
+     * @apiName find
+     * @apiGroup BottleAction
+     * @apiDescription Get the list of all bottle actions
+     *
+     * @apiUse forbiddenError
+     *
+     * @apiSuccess {Array} Array An array of bottles actions
+     * @apiSuccess {BottleAction} Array.bottleAction A bottle action object
+     * @apiSuccess {string} Array.bottle.team Team which produced a bottle action
+     * @apiSuccess {id} Array.bottle.bottleId Id of the concerned bottle
+     * @apiSuccess {integer} Array.bottle.quantity Number of bottles sold or moved (can be negative)
+     * @apiSuccess {string} Array.bottle.operation Operation performed on the bottle (sold or moved)
+     */
+
+    find: function(req, res) {
+	    // Check permissions
+        if((Team.can(req, 'bottleAction/read') && Team.param('id') == req.team('id')) ||
+            Team.can(req, 'bottleAction/admin')) {
+            BottleAction.find()
+                .exec((error, bottleAction) => {
+                    if (error) {
+                        return res.negotiate(error);
+                    }
+
+                    if(!(Team.can(req, 'bottleAction/admin'))) {
+                        let channel = 'BottleAction/' + req.team.id;
+                        sails.sockets.join(channel);
+                        return res.ok(botleAction);
+                    }
+                    else {
+                        BottleAction.watch(req);
+                        return res.ok(bottleAction);
+                    }
+                })
+        }
+        else {
+            return res.error(403, 'forbidden', 'You are not authorized to view the bottle actions list.');
+        }
+    },
+
+
+
+    findOne: function (req, res) {
+        // Check permissions
+        if(Team.can(req, 'bottleAction/read') || Team.can(req, 'bottleAction/admin')) {
+
+            // Find bottleAction
+            BottleAction.findOne({id: req.param('id')})
+                .exec((error, bottleAction) => {
+                    if (error) {
+                        return res.negotiate(error);
+                    }
+                    if(!bottleAction) {
+                        return res.error(400, 'BadRequest', 'The requested bottle action cannot be found');
+                    }
+
+                    BottleAtion.subscribe(req, [req.param('id')]);
+
+                    return res.ok(bottleAction);
+                });
+        }
+        else {
+            return res.error(403, 'forbidden', 'You are not authorized to read bottle action data');
+        }
+    },
+
+    create: function (req, res) {
+        // Check permissions
+        if(Team.can(req, 'bottleAction/create') || Team.can(req, 'bottleAction/admin')) {
+
+            // find bottleAction
+            BottleAction.findOne({id: req.param('id')}).exec((error, bottleAction) => {
+                if (bottleAction) {
+                    return res.error(400, 'BadRequest', 'Bottle action is not valid.');
+                }
+
+                // Create bottleAction
+                bottleAction = {};
+                if (req.param('team')) bottleAction.team = req.param('team');
+                if (req.param('bottleId')) bottleAction.bottleId = req.param('bottleId');
+                if (req.param('quantity')) bottleAction.quantity = req.param('quantity');
+                if (req.param('operation')) bottleAction.operation = 'purchased';
+
+                BottleAction.create(bottleAction).exec((error, bottleAction) => {
+                    if (error) {
+                        return res.negotiate(error);
+                    }
+
+                    Bottle.publishCreate(bottle);
+                    Bottle.subscribe(req, [bottle.id]);
+
+                    return res.ok(bottle);
+                });
+            })
+        }
+    },
+};
+
---
+++ api/models/BottleAction.js
@@ -0,0 +1,36 @@
+/**
+ * BottleAction.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+      team : {
+          model: 'team',
+          required: true,
+      },
+
+      bottleId : {
+          type: 'id',
+          required: true,
+      },
+
+      quantity : {
+          type: 'integer',
+          required: true,
+          defaultsTo: '1',
+      },
+
+      operation : {
+          type: 'string',
+          enum: ['purchased', 'moved'],
+      },
+
+  },
+
+};
+
