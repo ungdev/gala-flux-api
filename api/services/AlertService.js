4dc958d1e8897d9c286135dc9d64842d243d28b7
prudywsh <antoineprudhomme5@gmail.com>
2017-05-14 16:21:24 +0200

    checkTeamActivity now look at the sessions

--- api/services/AlertService.js
+++ api/services/AlertService.js
@@ -2,6 +2,8 @@ module.exports = {
 
     checkTeamActivity: function(teamId) {
 
+        let active = false;
+
         // get the team
         Team.findOne({ id: teamId }).exec((error, team) => {
             if (error || !team) return false;
@@ -17,43 +19,67 @@ module.exports = {
                     category: "Deconnexion"
                 };
 
-                // check if a user of this team is active
-                for (let user of users) {
+                async.each(users, (user, callback) => {
+                    // get the sessions of this user
+                    Session.find({user: user.id}).exec((err, sessions) => {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            if (!sessions.length) {
+                                callback();
+                            } else {
+                                for (let session of sessions) {
+                                    // if this session is active, check if there is an alert to remove for this team
+                                    if (session.lastAction >= session.disconnectedAt) {
+                                        active = true;
+                                        // if an alert has been sent for this team, destroy it
+                                        Alert.findOne(alertData)
+                                            .exec((error, alert) => {
+                                                if (error) {
+                                                    callback(error);
+                                                } else if (!alert) {
+                                                    callback();
+                                                } else {
+                                                    Alert.destroy({id: alert.id}).exec((error) => {
+                                                        if (error) {
+                                                            callback(error);
+                                                        } else {
+                                                            callback();
+                                                        }
+                                                    });
+                                                }
+                                            });
+                                    } else {
+                                        callback();
+                                    }
+                                }
 
-                    if (user.lastConnection > user.lastDisconnection) {
-
-                        // check if an alert has been sent for this team
-                        Alert.findOne(alertData)
-                            .exec((error, alert) => {
-                                if (error || !alert) return;
-
-                                Alert.destroy({id: alert.id}).exec((error) => {
-                                    if (error) return;
-                                });
-                            });
-                        return true;
-                    }
-                }
-
-                // an alert has to be created
-
-                // check if an alert for this team already exist
-                Alert.findOne(alertData)
-                    .exec((error, alert) => {
-                        // if there is no alert for this team, create one
-                        if (!alert) {
-                            Alert.create(alertData)
-                                .exec((error, alert) => {
-                                    if (error) return;
-
-                                    // push this modification in the alert history
-                                    AlertHistory.pushToHistory(alert, (error, result) => {
-                                        if (error) return false;
-                                    });
-
-                                });
+                            }
                         }
                     });
+                }, err => {
+
+                    // an error occured
+                    if (err) return err;
+
+                    // no error => if the team isn't active, an alert is required
+                    if (!active) {
+                        // check if an alert for this team already exist
+                        Alert.findOne(alertData).exec((error, alert) => {
+                            if (error || alert) return;
+                            // if there is no alert for this team, create one
+                            Alert.create(alertData).exec((error, alert) => {
+                                if (error) return;
+
+                                // push this modification in the alert history
+                                AlertHistory.pushToHistory(alert, (error, result) => {
+                                    if (error) return false;
+                                });
+
+                            });
+                        });
+                    }
+                });
 
             });
         });
