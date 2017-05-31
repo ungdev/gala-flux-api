6389afa1b951f0847cd4bfdb661262a2df173783
Aurélien Labate <aurelien@labate.me>
2017-05-31 20:42:34 +0200

    Set socket io origin according to cors configuration

--- config/cors.js
+++ config/cors.js
@@ -2,6 +2,8 @@
  * This configuration file is automatically injected as options for Express cors
  * middleware. So take a look at https://github.com/expressjs/cors to find
  * available options
+ *
+ * Note that origin attribute is also used to configure socket.io
  */
 
 module.exports = {
